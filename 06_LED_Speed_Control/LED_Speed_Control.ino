3eb23c4b98933ce2aaaa71afde8bd59d986090a0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-30 02:18:15 +0530

    LED Blink Speed Control using Potentiometer

---
+++ 06_LED_Speed_Control/LED_Speed_Control.ino
@@ -0,0 +1,37 @@
+const int POT_PIN = 34; // Input from potentiometer knob
+const int LED_PIN = 2;  // Built-in LED
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(POT_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+    // Read the potentiometer knob position
+    int potValue = analogRead(POT_PIN);
+
+    // Map the value to a delay (in milliseconds)
+    // Higher potValue -> Lower delay (faster blinking)
+    // We will use the `map()` function. This is a math helper function that converts one range of numbers into another
+    int blinkSpeed = map(potValue, 0, 4095, 1000, 50);
+
+    Serial.print("Potentiometer: ");
+    Serial.print(potValue);
+    Serial.print(" | Delay: ");
+    Serial.print(blinkSpeed);
+    Serial.println("ms");
+
+    // Perform the blink
+    blink(100);
+}
+
+void blink(int blinkSpeed)
+{
+    digitalWrite(LED_PIN, HIGH);
+    delay(blinkSpeed);
+    digitalWrite(LED_PIN, LOW);
+    delay(blinkSpeed);
+}
